b81467ab12c19610ea0e91c38264a1abb1b88d20
Jason Viger <jason.viger@gmail.com>
2018-08-19 06:53:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,7 +53,7 @@ class Song
     song
   end
   
-  def self.create_from_filename(name)
+  def self.create_from_filename(filename)
     song_details = []
     song_details = filename.split(/ - |\./)
     song = self.create
